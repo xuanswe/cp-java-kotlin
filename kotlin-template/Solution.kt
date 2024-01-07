e833f40645eeab332fc6b9b2dc2edd65354a3f9d
xuan-nguyen-swe <24372782+xuan-nguyen-swe@users.noreply.github.com>
2024-01-07 22:17:37 +0100

    Simplify kotlin template

--- kotlin-template/Solution.kt
+++ kotlin-template/Solution.kt
@@ -9,30 +9,20 @@ fun solve() {
   with(output) {
     // TODO: parse input, solve the problem and print result here
 
-    // Reading from input stream
-    // then printing result to output stream
+    // Reading the next line and printing directly to PrintStream
     println(nextLine.replace("input", "output"))
 
     // Reading an array and printing directly to PrintStream
-    IntArray(3) { next() }.joinTo(this, ";")
+    IntArray(3) { next() }.joinTo(this)
     println()
 
     // Reading a collection with a known size
     List<Int>(3) { next() }.joinTo(this, ";")
     println()
 
-    // Reading a collection with unknown size.
-    // The first word could lie on another line
-    // if the current line has only spaces unprocessed.
-    // Terminating at the first EOL after the first word,
-    // or when the `limit` is reached.
-    nextSequence<Int>(2).toList().joinTo(this, ";")
-    println()
-    nextSequence<Int>(10).toList().joinTo(this, ";")
-    println()
-
-    // Same as previously, but without considering `limit`.
-    // Terminating only at the first EOL after the first word.
+    // Read the next line, split into a sequence of words,
+    // and convert to a specific data type.
+    // Then convert the sequence to a collection.
     nextSequence<Int>().toList().joinTo(this, ";", postfix = "\n")
 
     // PrintStream supports formatting output directly
@@ -56,9 +46,9 @@ var output: PrintStream = System.out
   private set
 var logger: PrintStream? = null
   private set
-private val emptyTokenizer = StringTokenizer("")
-private var currentLine = emptyTokenizer
-private var lines = input.bufferedReader(Charsets.ISO_8859_1)
+private var currentLine = StringTokenizer("")
+private var lines = lines()
+private fun lines() = input.bufferedReader(Charsets.ISO_8859_1)
   .lineSequence().filter { it.isNotBlank() }.iterator()
 
 fun reset(
@@ -70,9 +60,8 @@ fun reset(
   output = outStream as? PrintStream ?: PrintStream(outStream, true)
   logger = loggerStream
 
-  currentLine = emptyTokenizer
-  lines = input.bufferedReader(Charsets.ISO_8859_1)
-    .lineSequence().filter { it.isNotBlank() }.iterator()
+  currentLine = StringTokenizer("")
+  lines = lines()
 }
 
 // endregion
@@ -88,14 +77,6 @@ val nextWord: String
     return currentLine.nextToken()
   }
 inline val nextWordOrNull get() = if (hasNextWord) nextWord else null
-fun nextSequenceOfWords(limit: Int = 0) = sequence {
-  var wordCount = 0
-  while ((wordCount == 0 && hasNextWord) || currentLineHasNextWord) {
-    yield(nextWord)
-    wordCount++
-    if (limit in 1..wordCount) return@sequence
-  }
-}
 
 // endregion
 
@@ -133,9 +114,9 @@ inline fun <reified T> String.to() = when (val type = T::class) {
 
 inline fun <reified T> nextOrNull() = if (hasNextWord) next<T>() else null
 inline fun <reified T> next(): T = nextWord.to()
-inline fun <reified T> nextSequence(limit: Int = 0) =
-  nextSequenceOfWords(limit).map { it.to<T>() }
-
-// endregion
+inline fun <reified T> nextSequence() = sequence<T> {
+  if (hasNextWord) yield(nextWord.to())
+  while (currentLineHasNextWord) yield(nextWord.to())
+}
 
 // endregion
